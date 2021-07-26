4c705bef4777e0f81b7e3789dadaec02e5cc6fdc
arturo <arturplur@gmail.com>
2021-07-26 16:42:13 -0500

    process files added

--- app.sh
+++ app.sh
@@ -3,4 +3,45 @@
 # sudo apt install libgconf-2-4 libatk1.0-0 libatk-bridge2.0-0 libgdk-pixbuf2.0-0 libgtk-3-0 libgbm-dev libnss3-dev libxss-dev
 # yarn add @mermaid-js/mermaid-cli
 
-/home/arturplur/node_modules/.bin/mmdc -i /home/arturplur/projects/analytics_documentation/example.mmd -o ./output/image.png
+#environment vars
+. ./env.sh
+
+echo "Welcome!"
+echo ""
+echo "Type one of the following:"
+echo "- example and then check the output folder."
+echo "- tag for tagging.mmd"
+echo "- test for testing.mmd"
+echo "- rep for report.mmd"
+echo "- exit, to exit."
+
+while :
+do
+	read INPUT_STRING
+	case $INPUT_STRING in
+		tag)
+			$ROOT_FOLDER/node_modules/.bin/mmdc -i $PROJECT_ROOT_FOLDER/analytics_documentation/input/tagging.mmd -o ./output/tagging.png
+			break
+            ;;
+		test)
+			echo "File testing.mmd not yet available."
+			;;
+		rep)
+			echo "File report.mmd not yet available."
+			break
+			;;
+        example)
+			$ROOT_FOLDER/node_modules/.bin/mmdc -i $PROJECT_ROOT_FOLDER/analytics_documentation/example.mmd -o ./output/image.png
+			break
+			;;
+        exit)
+            break
+            ;;
+        *)
+            echo "That's not a process. Bye."
+            break
+            ;;
+	esac
+done
+echo
+echo "That's all folks!"
